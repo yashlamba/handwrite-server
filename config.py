9649e9115af3f64f137d4bd9b427c9b2c63c0e5f
yashlamba <yashlamba2000@gmail.com>
2021-08-29 15:40:38 +0530

    Added basic logging structure

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
+from setup_logging import setup_logger
 import threading
+import logging
+
+from setup_logging import setup_logger
 from background import handwrite_background
 
 
 def on_starting(server):
+    logger = logging.getLogger("background")
+    setup_logger(logger)
     t = threading.Thread(target=handwrite_background)
     t.start()
+    logger.info("Background Service Thread Started")
